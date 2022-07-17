fac3054b7933b5c2ae3be7e93a4a67b9a1087b29
shellyguns <talleyranv@gmail.com>
2022-07-17 19:26:09 +0300

    added all functions

--- app/strings/words_quantity/words_quantity.py
+++ app/strings/words_quantity/words_quantity.py
@@ -3,12 +3,3 @@ def words_quantity(words: str):
         raise TypeError(f'Argument "words" must be string, not {type(words)}')
     words_list = words.split()
     return len(words_list)
-
-"""
-    spaces = 0
-    for space in words:
-        if space == " ":
-            spaces += 1
-    words_q = spaces + 1
-    return words_q
-"""
